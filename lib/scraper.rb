dd36af9ea2693bbf8dcf444c85643b0e1fee1172
Malcom Fetus <sora.vista@gmail.com>
2020-05-14 03:13:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +15,21 @@ class Scraper
       end
     end
   end
-  
+
+
+  def get_page
+doc = Nokogiri::HTML (open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+def get_courses
+self.get_page.css(".post")
+
 end
 
+def make_courses
+get_courses.each do |course|
+  course
+end
+end
 
-
+end
